7612a5f0ec6eddda26e91a52dcd562c9c65f59df
Benedict Golpeo <alanbenedictgolpeo@gmail.com>
2023-08-17 10:01:04 +0800

    adding blank template

---
+++ wp-content/themes/alan-wfx-challenge-theme/inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * alan-wfx-challenge-theme Theme Customizer
+ *
+ * @package alan-wfx-challenge-theme
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function alan_wfx_challenge_theme_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'alan_wfx_challenge_theme_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'alan_wfx_challenge_theme_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'alan_wfx_challenge_theme_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function alan_wfx_challenge_theme_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function alan_wfx_challenge_theme_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function alan_wfx_challenge_theme_customize_preview_js() {
+	wp_enqueue_script( 'alan-wfx-challenge-theme-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'alan_wfx_challenge_theme_customize_preview_js' );
